b03d55995b185bae6624a29c0cfbc6917e075fce
piotrmigas <piotr.migas@icloud.com>
2021-07-14 01:35:38 +0200

    webhook

--- pages/api/webhook.js
+++ pages/api/webhook.js
@@ -14,7 +14,7 @@ const stripe = require("stripe")(process.env.NEXT_PUBLIC_STRIPE_SECRET_KEY);
 const endpointSecret = process.env.NEXT_PUBLIC_STRIPE_SIGNING_SECRET;
 
 const fulfillOrder = async (session) => {
-  return app
+  return await app
     .firestore()
     .collection("users")
     .doc(session.metadata.email)
